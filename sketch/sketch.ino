3040a230a35ec4e407070ce6f978e9e68453a7bb
MrDrummer25 <mrdrummer.programming@hotmail.com>
2018-12-20 15:48:00 +0000

    multi strip control

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -4,6 +4,7 @@
 #endif
 
 #define PIN 6
+#define PIN_2 5
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -14,6 +15,7 @@
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(60, PIN_2, NEO_GRB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
@@ -31,13 +33,11 @@ void setup() {
   Serial.begin(9600);
 //  turnPixelsOff(wait);
   strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
   strip.setBrightness(64);
-//  strip.setPixelColor(0, 0, 255, 0);
   strip.show(); // Initialize all pixels to 'off'
-//  getPixelColours();
-//  turnPixelsOff(50);
-//  getPixelColours();
+  strip2.begin();
+  strip2.setBrightness(64);
+  strip2.show(); // Initialize all pixels to 'off'
   
 }
 
@@ -46,18 +46,26 @@ void setup() {
 //}
 void loop() {
   // Some example procedures showing how to display to the pixels:
-  // colorWipe(strip.Color(255, 0, 0), 50); // Red
-  // colorWipe(strip.Color(0, 255, 0), 50); // Green
-  // colorWipe(strip.Color(0, 0, 255), 50); // Blue
+  colorWipe(strip.Color(255, 0, 0), 50); // Red
+  colorWipe(strip.Color(0, 255, 0), 50); // Green
+  colorWipe(strip.Color(0, 0, 255), 50); // Blue
+  strip2_colorWipe(strip2.Color(255, 0, 0), 50); // Red
+  strip2_colorWipe(strip2.Color(0, 255, 0), 50); // Green
+  strip2_colorWipe(strip2.Color(0, 0, 255), 50); // Blue
 //colorWipe(strip.Color(0, 0, 0, 255), 50); // White RGBW
   // Send a theater pixel chase in...
   // theaterChase(strip.Color(127, 127, 127), 50); // White
-  // theaterChase(strip.Color(127, 0, 0), 50); // Red
-  // theaterChase(strip.Color(0, 0, 127), 50); // Blue
+  theaterChase(strip.Color(127, 0, 0), 50); // Red
+  theaterChase(strip.Color(0, 0, 127), 50); // Blue
+  strip2_theaterChase(strip2.Color(127, 0, 0), 50); // Red
+  strip2_theaterChase(strip2.Color(0, 0, 127), 50); // Blue
 
-  rainbow(20);
-  rainbowCycle(20);
+  rainbow(40);
+  rainbowCycle(40);
   theaterChaseRainbow(50);
+  strip2_rainbow(40);
+  strip2_rainbowCycle(40);
+  strip2_theaterChaseRainbow(50);
 }
 
 void getPixelColours() {
@@ -88,6 +96,16 @@ void colorWipe(uint32_t c, uint8_t wait) {
     delay(wait);
   }
 }
+// Fill the dots one after the other with a color
+void strip2_colorWipe(uint32_t c, uint8_t wait) {
+  Serial.println(c);
+  for(uint16_t i=0; i<strip2.numPixels(); i++) {
+    Serial.println(i);
+    strip2.setPixelColor(i, c);
+    strip2.show();
+    delay(wait);
+  }
+}
 
 void rainbow(uint8_t wait) {
   uint16_t i, j;
@@ -97,6 +115,18 @@ void rainbow(uint8_t wait) {
       strip.setPixelColor(i, Wheel((i+j) & 255));
     }
     strip.show();
+    delay(wait);
+  }
+}
+
+void strip2_rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip2.numPixels(); i++) {
+      strip2.setPixelColor(i, strip2_Wheel((i+j) & 255));
+    }
+    strip2.show();
     delay(wait);
   }
 }
@@ -114,6 +144,19 @@ void rainbowCycle(uint8_t wait) {
   }
 }
 
+// Slightly different, this makes the rainbow equally distributed throughout
+void strip2_rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip2.numPixels(); i++) {
+      strip2.setPixelColor(i, strip2_Wheel(((i * 256 / strip2.numPixels()) + j) & 255));
+    }
+    strip2.show();
+    delay(wait);
+  }
+}
+
 //Theatre-style crawling lights.
 void theaterChase(uint32_t c, uint8_t wait) {
   for (int j=0; j<10; j++) {  //do 10 cycles of chasing
@@ -127,6 +170,24 @@ void theaterChase(uint32_t c, uint8_t wait) {
 
       for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
         strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights.
+void strip2_theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip2.numPixels(); i=i+3) {
+        strip2.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip2.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip2.numPixels(); i=i+3) {
+        strip2.setPixelColor(i+q, 0);        //turn every third pixel off
       }
     }
   }
@@ -150,6 +211,24 @@ void theaterChaseRainbow(uint8_t wait) {
   }
 }
 
+//Theatre-style crawling lights with rainbow effect
+void strip2_theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip2.numPixels(); i=i+3) {
+        strip2.setPixelColor(i+q, strip2_Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip2.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip2.numPixels(); i=i+3) {
+        strip2.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
@@ -163,4 +242,19 @@ uint32_t Wheel(byte WheelPos) {
   }
   WheelPos -= 170;
   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t strip2_Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip2.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip2.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip2.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
