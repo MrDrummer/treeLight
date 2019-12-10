2f87c7774ea5f1c4c2ef39ffedcb83cdebcdd159
MrDrummer25 <mrdrummer.programming@hotmail.com>
2019-12-10 15:28:30 +0000

    matt

--- controller/controller.ino
+++ controller/controller.ino
@@ -4,6 +4,7 @@
 #endif
 
 #define PIN 6
+#define PIN_2 5
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -13,23 +14,32 @@
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-Adafruit_NeoPixel strip_1 = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(60, PIN_2, NEO_GRB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
 // and minimize distance between Arduino and first pixel.  Avoid connecting
 // on a live circuit...if you must, connect GND first.
 uint16_t wait = 1000;
-char mode = 1;
 
 void setup()
 {
+// This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+#if defined(__AVR_ATtiny85__)
+  if (F_CPU == 16000000)
+    clock_prescale_set(clock_div_1);
+#endif
+  // End of trinket special code
 
   Serial.begin(9600);
   //  turnPixelsOff(wait);
-  strip_1.begin();
-  strip_1.setBrightness(64);
-  strip_1.show(); // Initialize all pixels to 'off'
+  strip.begin();
+  strip.setBrightness(64);
+  strip.show(); // Initialize all pixels to 'off'
+  strip2.begin();
+  strip2.setBrightness(64);
+  strip2.show(); // Initialize all pixels to 'off'
 }
 
 //void loop() {
@@ -37,67 +47,30 @@ void setup()
 //}
 void loop()
 {
-  Serial.println("Current Mode:");
-  Serial.println(mode);
-  switch (mode)
-  {
-  // SPEED (last arg) could be controlled via interface
-  case 1: // wipe
-    // Some example procedures showing how to display to the pixels:
-    colorWipe(strip_1, strip_1.Color(255, 0, 0), 50); // Red
-    colorWipe(strip_1, strip_1.Color(0, 255, 0), 50); // Green
-    colorWipe(strip_1, strip_1.Color(0, 0, 255), 50); // Blue
-    break;
-  case 2:                                                // chase
-    theaterChase(strip_1, strip_1.Color(127, 0, 0), 50); // Red
-    theaterChase(strip_1, strip_1.Color(0, 127, 0), 50); // Green
-    theaterChase(strip_1, strip_1.Color(0, 0, 127), 50); // Blue
-    theaterChaseRainbow(strip_1, 50);
-    break;
-  case 3: // rainbow
-    rainbow(strip_1, 50);
-    rainbowCycle(strip_1, 50);
-    theaterChaseRainbow(strip_1, 50);
-    break;
-  default:
-    Serial.println("no mode:" + mode);
-    delay(10000);
-    break;
-  }
+  // Some example procedures showing how to display to the pixels:
+  colorWipe(strip.Color(255, 0, 0), 50); // Red
+  colorWipe(strip.Color(0, 255, 0), 50); // Green
+  colorWipe(strip.Color(0, 0, 255), 50); // Blue
+  //strip2_colorWipe(strip2.Color(255, 0, 0), 50); // Red
+  //strip2_colorWipe(strip2.Color(0, 255, 0), 50); // Green
+  //strip2_colorWipe(strip2.Color(0, 0, 255), 50); // Blue
   //colorWipe(strip.Color(0, 0, 0, 255), 50); // White RGBW
   // Send a theater pixel chase in...
   // theaterChase(strip.Color(127, 127, 127), 50); // White
+  //theaterChase(strip.Color(127, 0, 0), 50);         // Red
+  //theaterChase(strip.Color(0, 0, 127), 50);         // Blue
+  //strip2_theaterChase(strip2.Color(127, 0, 0), 50); // Red
+  //strip2_theaterChase(strip2.Color(0, 0, 127), 50); // Blue
+
+  rainbow(40);
+  // rainbowCycle(40);
+  // theaterChaseRainbow(50);
+  //strip2_rainbow(40);
+  //strip2_rainbowCycle(40);
+  //strip2_theaterChaseRainbow(50);
 }
 
-bool checkForInterrupt()
-{
-  if (Serial.available() > 0)
-  {                                      //From RPi to Arduino
-    mode = mode * (Serial.read() - '0'); //conveting the value of chars to integer
-    Serial.println("=============");
-    Serial.println(mode);
-    // switch (r) {
-    //   case "wipe":
-    //     mode = 1
-    //     break
-    //   case "chase":
-    //     mode = 2
-    //     break
-    //   case "rainbow":
-    //     mode = 3
-    //     break
-
-    //   default:
-    //     break
-    // }
-  }
-  else
-  {
-    Serial.println("No serial Buffer");
-  }
-}
-
-void getPixelColours(Adafruit_NeoPixel strip)
+void getPixelColours()
 {
   for (uint16_t i = 0; i < strip.numPixels(); i++)
   {
@@ -107,7 +80,7 @@ void getPixelColours(Adafruit_NeoPixel strip)
 }
 
 // Fill the dots one after the other with a color
-void turnPixelsOff(Adafruit_NeoPixel strip, uint16_t wait)
+void turnPixelsOff(uint16_t wait)
 {
   uint32_t colour = strip.Color(0, 255, 0);
   Serial.println(wait);
@@ -116,29 +89,37 @@ void turnPixelsOff(Adafruit_NeoPixel strip, uint16_t wait)
     Serial.println(i);
     Serial.println(colour);
     //    strip.setPixelColor(i-1, colour);
-    if (checkForInterrupt())
-      return;
     strip.show();
     delay(wait);
   }
 }
 
 // Fill the dots one after the other with a color
-void colorWipe(Adafruit_NeoPixel strip, uint32_t c, uint8_t wait)
+void colorWipe(uint32_t c, uint8_t wait)
 {
-  // Serial.println(c);
+  Serial.println(c);
   for (uint16_t i = 0; i < strip.numPixels(); i++)
   {
-    // Serial.println(i);
+    Serial.println(i);
     strip.setPixelColor(i, c);
-    if (checkForInterrupt())
-      return;
     strip.show();
     delay(wait);
   }
 }
+// Fill the dots one after the other with a color
+void strip2_colorWipe(uint32_t c, uint8_t wait)
+{
+  Serial.println(c);
+  for (uint16_t i = 0; i < strip2.numPixels(); i++)
+  {
+    Serial.println(i);
+    strip2.setPixelColor(i, c);
+    strip2.show();
+    delay(wait);
+  }
+}
 
-void rainbow(Adafruit_NeoPixel strip, uint8_t wait)
+void rainbow(uint8_t wait)
 {
   uint16_t i, j;
 
@@ -146,17 +127,30 @@ void rainbow(Adafruit_NeoPixel strip, uint8_t wait)
   {
     for (i = 0; i < strip.numPixels(); i++)
     {
-      strip.setPixelColor(i, Wheel(strip, (i + j) & 255));
+      strip.setPixelColor(i, Wheel((i + j) & 255));
     }
-    if (checkForInterrupt())
-      return;
     strip.show();
     delay(wait);
   }
 }
 
+void strip2_rainbow(uint8_t wait)
+{
+  uint16_t i, j;
+
+  for (j = 0; j < 256; j++)
+  {
+    for (i = 0; i < strip2.numPixels(); i++)
+    {
+      strip2.setPixelColor(i, strip2_Wheel((i + j) & 255));
+    }
+    strip2.show();
+    delay(wait);
+  }
+}
+
 // Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(Adafruit_NeoPixel strip, uint8_t wait)
+void rainbowCycle(uint8_t wait)
 {
   uint16_t i, j;
 
@@ -164,17 +158,31 @@ void rainbowCycle(Adafruit_NeoPixel strip, uint8_t wait)
   { // 5 cycles of all colors on wheel
     for (i = 0; i < strip.numPixels(); i++)
     {
-      strip.setPixelColor(i, Wheel(strip, ((i * 256 / strip.numPixels()) + j) & 255));
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
     }
-    if (checkForInterrupt())
-      return;
     strip.show();
     delay(wait);
   }
 }
 
+// Slightly different, this makes the rainbow equally distributed throughout
+void strip2_rainbowCycle(uint8_t wait)
+{
+  uint16_t i, j;
+
+  for (j = 0; j < 256 * 5; j++)
+  { // 5 cycles of all colors on wheel
+    for (i = 0; i < strip2.numPixels(); i++)
+    {
+      strip2.setPixelColor(i, strip2_Wheel(((i * 256 / strip2.numPixels()) + j) & 255));
+    }
+    strip2.show();
+    delay(wait);
+  }
+}
+
 //Theatre-style crawling lights.
-void theaterChase(Adafruit_NeoPixel strip, uint32_t c, uint8_t wait)
+void theaterChase(uint32_t c, uint8_t wait)
 {
   for (int j = 0; j < 10; j++)
   { //do 10 cycles of chasing
@@ -184,10 +192,54 @@ void theaterChase(Adafruit_NeoPixel strip, uint32_t c, uint8_t wait)
       {
         strip.setPixelColor(i + q, c); //turn every third pixel on
       }
-
-      if (checkForInterrupt())
-        return;
       strip.show();
+
+      delay(wait);
+
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3)
+      {
+        strip.setPixelColor(i + q, 0); //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights.
+void strip2_theaterChase(uint32_t c, uint8_t wait)
+{
+  for (int j = 0; j < 10; j++)
+  { //do 10 cycles of chasing
+    for (int q = 0; q < 3; q++)
+    {
+      for (uint16_t i = 0; i < strip2.numPixels(); i = i + 3)
+      {
+        strip2.setPixelColor(i + q, c); //turn every third pixel on
+      }
+      strip2.show();
+
+      delay(wait);
+
+      for (uint16_t i = 0; i < strip2.numPixels(); i = i + 3)
+      {
+        strip2.setPixelColor(i + q, 0); //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait)
+{
+  for (int j = 0; j < 256; j++)
+  { // cycle all 256 colors in the wheel
+    for (int q = 0; q < 3; q++)
+    {
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3)
+      {
+        strip.setPixelColor(i + q, Wheel((i + j) % 255)); //turn every third pixel on
+      }
+      strip.show();
+
       delay(wait);
 
       for (uint16_t i = 0; i < strip.numPixels(); i = i + 3)
@@ -199,25 +251,23 @@ void theaterChase(Adafruit_NeoPixel strip, uint32_t c, uint8_t wait)
 }
 
 //Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(Adafruit_NeoPixel strip, uint8_t wait)
+void strip2_theaterChaseRainbow(uint8_t wait)
 {
   for (int j = 0; j < 256; j++)
   { // cycle all 256 colors in the wheel
     for (int q = 0; q < 3; q++)
     {
-      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3)
+      for (uint16_t i = 0; i < strip2.numPixels(); i = i + 3)
       {
-        strip.setPixelColor(i + q, Wheel(strip, (i + j) % 255)); //turn every third pixel on
+        strip2.setPixelColor(i + q, strip2_Wheel((i + j) % 255)); //turn every third pixel on
       }
+      strip2.show();
 
-      if (checkForInterrupt())
-        return;
-      strip.show();
       delay(wait);
 
-      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3)
+      for (uint16_t i = 0; i < strip2.numPixels(); i = i + 3)
       {
-        strip.setPixelColor(i + q, 0); //turn every third pixel off
+        strip2.setPixelColor(i + q, 0); //turn every third pixel off
       }
     }
   }
@@ -225,7 +275,7 @@ void theaterChaseRainbow(Adafruit_NeoPixel strip, uint8_t wait)
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
-uint32_t Wheel(Adafruit_NeoPixel strip, byte WheelPos)
+uint32_t Wheel(byte WheelPos)
 {
   WheelPos = 255 - WheelPos;
   if (WheelPos < 85)
@@ -239,4 +289,22 @@ uint32_t Wheel(Adafruit_NeoPixel strip, byte WheelPos)
   }
   WheelPos -= 170;
   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t strip2_Wheel(byte WheelPos)
+{
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85)
+  {
+    return strip2.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170)
+  {
+    WheelPos -= 85;
+    return strip2.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip2.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
